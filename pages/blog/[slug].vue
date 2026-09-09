97a6a95539834d849f91b5b341452f36efada128
stefan van der Weide <stefanvanderweide@Mac.home>
2026-09-09 22:00:26 +0200

    make images full width in blog body

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -2,12 +2,15 @@
   <div v-if="post" class="w-full lg:w-1/2 justify-self-center min-h-screen">
     <div class="mb-12">
       <h1 class="text-4xl font-extrabold">{{ post.title }}</h1>
-      <span class="text-gray-500 text-sm">Stefan van der Weide | {{ new
-        Date(post.date).toISOString().substring(0, 10)
-        }}</span>
+      <span class="text-gray-500 text-sm"
+        >Stefan van der Weide |
+        {{ new Date(post.date).toISOString().substring(0, 10) }}</span
+      >
     </div>
-    <ContentRenderer :value="post"
-      class="prose lg:prose-xl prose-invert prose-inline-code:font-normal prose-inline-code:p-1 prose-inline-code:border-[1px] prose-inline-code:border-solid prose-inline-code:rounded prose-inline-code:bg-black prose-inline-code:text-gray-400" />
+    <ContentRenderer
+      :value="post"
+      class="prose lg:prose-xl prose-invert prose-img:w-full prose-inline-code:font-normal prose-inline-code:p-1 prose-inline-code:border prose-inline-code:border-solid prose-inline-code:rounded prose-inline-code:bg-black prose-inline-code:text-gray-400"
+    />
   </div>
 </template>
 
@@ -17,11 +20,11 @@ const route = useRoute();
 
 // Fetch blog post data
 const { data: post } = await useAsyncData(route.path, () =>
-  queryCollection("blog").path(route.path).first()
+  queryCollection("blog").path(route.path).first(),
 );
 
 if (!post.value) {
-  throw createError({ statusCode: 404, statusMessage: 'Page Not Found' })
+  throw createError({ statusCode: 404, statusMessage: "Page Not Found" });
 }
 
 // Define the OgImage for this page from the content frontmatter
@@ -38,5 +41,5 @@ useSeoMeta({
   ogTitle: post.value?.title,
   description: post.value?.description,
   ogDescription: post.value?.description,
-})
+});
 </script>
